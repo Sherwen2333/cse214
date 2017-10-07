b944afd86ddae665e33d469541db225d5893ab3b
Zhaowen <h308120095@gmail.com>
2017-10-07 17:04:09 -0400

    asd

--- src/CSE214hw2_1.java
+++ src/CSE214hw2_1.java
@@ -2,80 +2,132 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-
 public class CSE214hw2_1 {
-public static void main(String[] args) throws FileNotFoundException {
-//	System.out.println("Fk That Shit");
-	File aFile = new File("in1.txt");
-	Scanner aScanner = new Scanner(aFile);
-	 for (int i=0;i<2;i++){
-		System.out.println(aScanner.nextLine());
-	}
-	String[] aStrings= aScanner.nextLine().split(" ");
-	String[] bStrings= aScanner.nextLine().split(" ");
-	System.out.println(aStrings.length+" "+bStrings.length);
-	for (String a: aStrings)
-	System.out.print(a+",");
-	System.out.println();
-	for (String a: bStrings)
-	System.out.print(a+",");
-	String[][] ak1=new String[4][2];
-	String[][] ak2=new String[3][2];
-	String[][] ak3=new String[3][2];
-
-	aScanner.close();
-	for (int i=0;i<aStrings.length-1;i++){
-		for (int a=i+1;a<aStrings.length;a++){
-			if (Integer.parseInt(bStrings[i])>Integer.parseInt(bStrings[a])){
-				String temp=bStrings[i];
-				bStrings[i]=bStrings[a];
-				bStrings[a]=temp;
-				temp=aStrings[i];
-				aStrings[i]=aStrings[a];
-				aStrings[a]=temp;
+	public static void main(String[] args) throws FileNotFoundException {
+		File aFile = new File("in1.txt");
+		Scanner aScanner = new Scanner(aFile);
+		int time = aScanner.nextInt();
+		aScanner.nextLine();
+		System.out.println(time);
+		for (int iz = 0; iz < time; iz++) {
+			String[] akb = aScanner.nextLine().split(" ");
+			int a_number = Integer.parseInt(akb[0]);
+			int b_number = Integer.parseInt(akb[1]);
+			ak<String> aak = new ak<>(aScanner.nextLine().split(" "));
+			ak<String> bak = new ak<>(aScanner.nextLine().split(" "));
+			System.out.println(a_number + " " + b_number);
+			String[] leftPeople = new String[b_number];
+			aak.print();
+			bak.print();
+//			System.out.println(bak.getSize());
+			for (int i = 0; i < aak.getSize() - 1; i++) {
+				for (int a = i + 1; a < aak.getSize(); a++) {
+					if (Integer.parseInt(bak.getter(a).getData()) < Integer
+							.parseInt(bak.getter(i).getData())) {
+						aak.Switch(aak.getter(a), aak.getter(i));
+						bak.Switch(bak.getter(a), bak.getter(i));
+					}
+				}
+			}
+			// Sort
+			aak.print();
+			bak.print();
+		System.out.println(	bak.getter(1).getData()+"-111");
+			System.out.println(bak.getSize());
+			int a=Integer.parseInt(bak.getFirst().getData());
+			//Spilt into K buckets
+			for (int i=0;i<b_number;i++) {
+				ak<String> az=new ak<String>();
+				ak<String> bz=new ak<String>();
+				int asd=bak.getSize();
+//				System.out.println(bak.getSize()+"--");
+					for (int isd =0;isd<asd;isd++) {
+						System.out.println(isd+"0/0");
+						if(Integer.parseInt(bak.getter(isd).getData())<a+b_number&&i!=b_number-1)
+						{
+							az.add(aak.getter(isd).getData());
+							bz.add(bak.getter(isd).getData());
+							aak.remove(aak.getter(isd).getData());
+							bak.remove(bak.getter(isd).getData());
+						}
+						else {
+							az=aak;
+							bz=bak;
+							break;
+						}
+					}
+					
+					String aString = az.getFirst().getData();
+					while (az.getSize()!=1) {
+						az.print();
+						System.out.println("---------");
+						for(int iaz=0;iaz<b_number-1;iaz++) {
+						aString=az.getter(aString).getNext().getData();}
+						String aString2=az.getter(aString).getNext().getData();
+						az.remove(aString);
+						aString=aString2;
+					}
+					leftPeople[i]=aString;
 			}
 		}
-	}
-	int indexa=0;
-	int indexb=0;
-	int indexc=0;	
-	for (int i=0;i<aStrings.length;i++){
-		if(Integer.parseInt(bStrings[i])<=4){
-			ak1[indexa][0]=aStrings[i];
-			ak1[indexa][1]=bStrings[i];
-			indexa++;
-		}
-		else if(Integer.parseInt(bStrings[i])<=7){
-			ak2[indexb][0]=aStrings[i];
-			ak2[indexb][1]=bStrings[i];
-			indexb++;
 
-		}
-		else if(Integer.parseInt(bStrings[i])<=11){
-			ak3[indexc][0]=aStrings[i];
-			ak3[indexc][1]=bStrings[i];
-			indexc++;
+		// for (int i=0;i<2;i++){
+		// System.out.println(aScanner.nextLine());
+		// }
+		// String[] aak= aScanner.nextLine().split(" ");
+		// String[] bStrings= aScanner.nextLine().split(" ");
+		// System.out.println(aak.length+" "+bStrings.length);
+		// for (String a: aak)
+		// System.out.print(a+",");
+		// System.out.println();
+		// for (String a: bStrings)
+		// System.out.print(a+",");
+		// String[][] ak1=new String[4][2];
+		// String[][] ak2=new String[3][2];
+		// String[][] ak3=new String[3][2];
+		//
+		// aScanner.close();
 
-		}
+		// int indexa=0;
+		// int indexb=0;
+		// int indexc=0;
+		// for (int i=0;i<aak.length;i++){
+		// if(Integer.parseInt(bStrings[i])<=4){
+		// ak1[indexa][0]=aak[i];
+		// ak1[indexa][1]=bStrings[i];
+		// indexa++;
+		// }
+		// else if(Integer.parseInt(bStrings[i])<=7){
+		// ak2[indexb][0]=aak[i];
+		// ak2[indexb][1]=bStrings[i];
+		// indexb++;
+		//
+		// }
+		// else if(Integer.parseInt(bStrings[i])<=11){
+		// ak3[indexc][0]=aak[i];
+		// ak3[indexc][1]=bStrings[i];
+		// indexc++;
+		//
+		// }
+		// }
+		// System.out.println();
+		// for (String[] a: ak1)
+		// System.out.print(a[0]+",");
+		// System.out.println();
+		// for (String[] a: ak1)
+		// System.out.print(a[1]+",");
+		// System.out.println();
+		// for (String[] a: ak2)
+		// System.out.print(a[0]+",");
+		// System.out.println();
+		// for (String[] a: ak2)
+		// System.out.print(a[1]+",");
+		// System.out.println();
+		// for (String[] a: ak3)
+		// System.out.print(a[0]+",");
+		// System.out.println();
+		// for (String[] a: ak3)
+		// System.out.print(a[1]+",");
+		// System.out.println();
 	}
-	System.out.println();
-	for (String[] a: ak1)
-		System.out.print(a[0]+",");
-	System.out.println();
-	for (String[] a: ak1)
-		System.out.print(a[1]+",");
-	System.out.println();
-	for (String[] a: ak2)
-		System.out.print(a[0]+",");
-	System.out.println();
-	for (String[] a: ak2)
-		System.out.print(a[1]+",");
-	System.out.println();
-	for (String[] a: ak3)
-		System.out.print(a[0]+",");
-	System.out.println();
-	for (String[] a: ak3)
-		System.out.print(a[1]+",");
-	System.out.println();
-}
 }
--- src/ak.java
+++ src/ak.java
@@ -1,6 +1,6 @@
 
 
-public class linkedlist<T>  {
+public class ak<T>  {
 	// aka
 	// asd
 	//asd
@@ -9,10 +9,12 @@ public class linkedlist<T>  {
 		private T data;
         private Node next;
         private Node prev;
-        public Node(Node prev,T data, Node next) {
+        private  int ak;
+        public Node(Node prev,T data, Node next,int a) {
             this.data = data;
             this.next = next;
             this.prev = prev;
+            this.ak=a;
         }
         public Node getNext() {
    		 return next;
@@ -26,53 +28,83 @@ public class linkedlist<T>  {
         	return false;
         }
         public boolean equal(Node a){
-        	if (a.data.equals(data)) return true;
+        	if (a.ak==ak) return true;
         	
         	return false;
         }
 	}
 	private Node first;
-    private Node Current;
 	private Node last;
-	  public linkedlist() {
+	public ak(T[] a)  {
+		for (int i =0;i<a.length;i++) {
+			add(a[i]);
+		}
+	}
+	  public ak() {
 	        first = null;
 	        last = null;
 	        a = 0;
 	    }
-	public Node getter() {
-		return first;
+	public Node getter(int a) {
+		Node aNode = first;
+		for (int i=0;i<a;i++) {
+			aNode=aNode.next;
+		}
+		return aNode;		
+	}
+	public Node getter(T a) {
+		Node aNode = first;
+		for (int i=0;i<getSize();i++) {
+			if(aNode.data.equals(a)) 
+				return aNode;		
+			aNode=aNode.next;
+		}
+		return aNode;
+	}
+	public void Switch(Node a,Node b) {
+//		System.out.println(a.data+"-*-"+b.data);
+		T temp=a.data;
+		a.data=b.data;
+		b.data=temp;
+//		System.out.println(a.data+"-*-"+b.data);
 	}
 	 public void add(T item) {
 		 if (a==0){
-			 first=new Node(first, item, first);
+			 first=new Node(first, item, first,0);
 			 last=first;
 			 a=1;
 		 }
 		 else{
 		 Node prev=last;
-		 Node aNode= new Node(last,item, first);
+		 Node aNode= new Node(last,item, first,(last.ak));
 		 prev.next=aNode;
 		 aNode.prev=prev;
-		 last=aNode;}
+		 last=aNode;
+		 last.ak+=1;
+		 }
 	    }
 	
 	 public int  getSize() {
 		if (first==null) return 0;
 		Node aNode=first;
 		int a=1;
-		if (aNode.equal(last)) return a;
+		if (last.equal(first)) return 1;
 		while(!aNode.next.equal(first)){
 			a++;
 			aNode=aNode.next;
 		}
 		return a;
 	}
+	 public Node getFirst() {
+		 return first;
+	}
 	 public void print() {
 		 Node a=first;
 		 for (int i=0;i<getSize();i++){
-			System.out.println(a.data);
+			System.out.print(a.data+" ");
 			a=a.next;
 		 }
+		 System.out.println();
 	}
 	 public void remove(T item) {
 		 if(getSize()==1){
